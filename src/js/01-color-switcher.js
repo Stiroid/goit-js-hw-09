b33d677dc0e69b8499f625f888fa215b31813041
Stiroid <stakhovsky89@gmail.com>
2023-03-14 19:49:42 +0200

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,32 +2,36 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const btnStart = document.querySelector('button[data-start]');
-const btnStop = document.querySelector('button[data-stop]');
-const interval = {
-  intervalId: null,
-  isActive: false,
-};
-let btnColor = null;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.body;
+let timerId = null;
 
-const stopChange = () => {
-  clearInterval(this.intervalId);
-  document.body.style.backgroundColor = '#fff';
-  this.isActive = false;
-};
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
 
-btnStop.addEventListener('click', stopChange);
+btnStop.disabled = true;
 
-const colorChange = () => {
-  if (this.interval.isActive) {
-    return;
-  }
-  this.intervalId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-    this.isActive = true;
-  }, 1000);
-};
-btnStart.addEventListener('click', colorChange);
+function onBtnStartClick(event) {
+    timerId = setInterval(changeColor, 1000);
+
+    event.target.disabled = true;
+    btnStop.disabled = false;
+}
+
+function onBtnStopClick(event) {
+    clearInterval(timerId);
+
+    event.target.disabled = true;
+    btnStart.disabled = false;
+
+}
+
+function changeColor() {
+    const colorRandom = getRandomHexColor();
+
+    bodyEl.style.backgroundColor = colorRandom;
+}
 
 btnStart.style.backgroundColor = '#e9c501';
 btnStop.style.backgroundColor = '#477ab5';
